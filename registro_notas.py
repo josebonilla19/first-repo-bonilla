7fd08cfa9115c1a67ebd0293a45a388925beb349
josebonilla19 <josebonilla@ufm.edu>
2026-08-05 12:13:07 -0600

    Agrega registro de notas de examen

---
+++ registro_notas.py
@@ -0,0 +1,18 @@
+contador = 0
+suma = 0
+ 
+nota = int(input("Ingresa una nota (o -1 para terminar): "))
+ 
+while nota != -1:
+    if nota < 0 or nota > 100:
+        print("Nota fuera de rango, intenta de nuevo.")
+    else:
+        contador = contador + 1
+        suma = suma + nota
+    nota = int(input("Ingresa una nota (o -1 para terminar): "))
+ 
+promedio = suma / contador
+ 
+print()
+print("Notas ingresadas:", contador)
+print("Promedio:", promedio)
